e1ef8b813b70aba21f6bc8ffea509ff0dfff349f
mepeng <872502023@qq.com>
2020-08-14 16:49:25 +0800

    commit

--- springcloud-eureka/src/main/java/com/mepeng/cn/SevenPen/eureka/EurekaServerApplication.java
+++ springcloud-eureka/src/main/java/com/mepeng/cn/SevenPen/eureka/EurekaServerApplication.java
@@ -10,11 +10,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
-    /**
-     * The entry point of application.
-     *
-     * @param args the input arguments
-     */
+
     public static void main(String[] args) {
         new SpringApplicationBuilder(EurekaServerApplication.class).run(args);
     }
